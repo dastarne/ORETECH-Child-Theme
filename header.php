18ea9cd3a76c2de2995aad6d0afb430c2e6f8697
tdoanUNCG <48730552+tdoanUNCG@users.noreply.github.com>
2019-04-23 11:41:51 -0400

    Update header.php

--- header.php
+++ header.php
@@ -7,22 +7,94 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 
-$site = $theme->get('site', []);
+// Set view
+if (is_home()|| is_category() || is_tag()) {
+    $view = 'blog';
+} elseif (is_singular('post')) {
+    $view = 'post';
+} else {
+    $view = '';
+}
+
+// Parameter shortcuts
+$site  = $theme->get('site', []);
+$blog_settings  = $theme->get('blog', []);
+$post_settings  = $theme->get('post', []);
+
+// Deprecated
+if ($site['boxed.padding'] && !isset($site['boxed.margin_top'], $site['boxed.margin_bottom'])) {
+    $site['boxed.margin_top'] = true;
+    $site['boxed.margin_bottom'] = true;
+}
 
 // Page
 $attrs_page = [];
 $attrs_page_container = [];
+$attrs_image = [];
+$attrs_media_overlay = [];
 
 $attrs_page['class'][] = 'tm-page';
 
 if ($site['layout'] == 'boxed') {
 
     $attrs_page['class'][] = $site['boxed.alignment'] ? 'uk-margin-auto' : '';
+    $attrs_page['class'][] = $site['boxed.margin_top'] ? 'tm-page-margin-top' : '';
+    $attrs_page['class'][] = $site['boxed.margin_bottom'] ? 'tm-page-margin-bottom' : '';
+    $attrs_page_container['class'][] = 'tm-page-container uk-clearfix';
 
-    $attrs_page_container['class'][] = 'tm-page-container';
-    $attrs_page_container['class'][] = $site['boxed.padding'] ? 'tm-page-container-padding' : '';
-    $attrs_page_container['style'][] = $site['boxed.media'] ? "background-image: url('{$theme->app->url($site['boxed.media'])}');" : '';
+    // Image
+    if ($site['boxed.media']) {
 
+        $attrs_image = $theme->app['view']->bgImage($site['boxed.media'], [
+            'width' => $site['image_width'],
+            'height' => $site['image_height'],
+            'size' => $site['image_size'],
+            'position' => $site['image_position'],
+            'visibility' => $site['image_visibility'],
+            'blend_mode' => $site['media_blend_mode'],
+            'background' => $site['media_background'],
+            'effect' => $site['image_effect'],
+            'parallax_bgx_start' => $site['image_parallax_bgx_start'],
+            'parallax_bgy_start' => $site['image_parallax_bgy_start'],
+            'parallax_bgx_end' => $site['image_parallax_bgx_end'],
+            'parallax_bgy_end' => $site['image_parallax_bgy_end'],
+            'parallax_easing' => $site['image_parallax_easing'],
+            'parallax_breakpoint' => $site['image_parallax_breakpoint'],
+            'parallax_target' => 'body',
+        ]);
+
+        if ($site['image_effect']) {
+            $attrs_image['class'][] = 'uk-position-cover uk-position-fixed';
+        } else {
+            $attrs_page_container = array_merge_recursive($attrs_page_container, $attrs_image);
+            $attrs_image = [];
+        }
+
+        // Overlay
+        if ($site['media_overlay']) {
+            $attrs_page_container['class'][] = 'uk-position-relative';
+            $attrs_media_overlay['class'][] = 'uk-position-cover';
+            $attrs_media_overlay['style'] = "background-color: {$site['media_overlay']};";
+        }
+
+    }
+
+}
+
+// Main section
+$attrs_main_section = [];
+$attrs_main_section['class'][] = 'tm-main uk-section uk-section-default';
+$attrs_main_section['class'][] = $view == 'blog' && $blog_settings['padding'] ? "uk-section-{$blog_settings['padding']}" : '';
+$attrs_main_section['class'][] = $view == 'post' && $post_settings['padding'] ? "uk-section-{$post_settings['padding']}" : '';
+$attrs_main_section['class'][] = $view == 'post' && $post_settings['padding_remove'] ? 'uk-padding-remove-top' : '';
+
+// Main container
+$attrs_main_container = [];
+
+if ($view !== 'post' || ($view == 'post' && $post_settings['width'] != 'none')) {
+    $attrs_main_container['class'][] = 'uk-container';
+    $attrs_main_container['class'][] = $view == 'blog' && $blog_settings['width'] ? "uk-container-{$blog_settings['width']}" : '';
+    $attrs_main_container['class'][] = $view == 'post' && $post_settings['width'] ? "uk-container-{$post_settings['width']}" : '';
 }
 
 ?>
@@ -38,76 +110,53 @@ if ($site['layout'] == 'boxed') {
         <link rel="pingback" href="<?php bloginfo('pingback_url') ?>">
         <?php endif ?>
         <?php wp_head() ?>
-		<script type="text/javascript" src="//fast.fonts.com/jsapi/17a574c9-e9d6-4264-b72e-cf2d9706cacb.js"></script>
-		<link type="text/css" rel="stylesheet" href="//fast.fonts.com/cssapi/17a574c9-e9d6-4264-b72e-cf2d9706cacb.css"/>
     </head>
     <body <?php body_class($theme->get('body_class')->all()) ?>>
-        <?php if (strpos($theme->get('header.layout'), 'offcanvas') === 0 || $theme->get('mobile.animation') == 'offcanvas') : ?>
-		<!-- Start rincuncg heading -->
-		<?php require_once('rincuncg/heading.php');?>
-		<!-- End rincuncg heading -->
-        <div class="uk-offcanvas-content">
-        <?php endif ?>
+    	<!-- Start rincuncg heading -->
+      <?php require_once('rincuncg/heading.php');?>
+      <!-- End rincuncg heading -->
 
         <?php if ($site['layout'] == 'boxed') : ?>
         <div<?= get_attrs($attrs_page_container) ?>>
+
+            <?php if ($attrs_image) : ?>
+            <div<?= get_attrs($attrs_image) ?>></div>
+            <?php endif ?>
+
+            <?php if ($attrs_media_overlay) : ?>
+            <div class="uk-position-cover"<?= get_attrs($attrs_media_overlay) ?>></div>
+            <?php endif ?>
+
+        <?php endif ?>
+
+        <?php if ($site['layout'] == 'boxed' && $site['boxed.header_outside']) : ?>
+        <?= get_view('header') ?>
         <?php endif ?>
 
         <div<?= get_attrs($attrs_page) ?>>
 
-            <div class="tm-header-mobile uk-hidden@<?= $theme->get('mobile.breakpoint') ?>">
-            <?= get_view('header-mobile') ?>
-            </div>
-
-            <?php if (is_active_sidebar('toolbar-left') || is_active_sidebar('toolbar-right')) : ?>
-            <div class="tm-toolbar uk-visible@<?= $theme->get('mobile.breakpoint') ?>">
-                <div class="uk-container uk-flex uk-flex-middle <?= $site['toolbar_fullwidth'] ? 'uk-container-expand' : '' ?> <?= $site['toolbar_center'] ? 'uk-flex-center' : '' ?>">
-
-                    <?php if (is_active_sidebar('toolbar-left') || ($site['toolbar_center'] && is_active_sidebar('toolbar-right'))) : ?>
-                    <div>
-                        <div class="uk-grid-medium uk-child-width-auto uk-flex-middle" uk-grid="margin: uk-margin-small-top">
-
-                            <?php if (is_active_sidebar('toolbar-left')) : ?>
-                            <?php dynamic_sidebar("toolbar-left:cell") ?>
-                            <?php endif ?>
-
-                            <?php if ($site['toolbar_center'] && is_active_sidebar('toolbar-right')) : ?>
-                            <?php dynamic_sidebar("toolbar-right:cell") ?>
-                            <?php endif ?>
-
-                        </div>
-                    </div>
-                    <?php endif ?>
-
-                    <?php if (!$site['toolbar_center'] && is_active_sidebar('toolbar-right')) : ?>
-                    <div class="uk-margin-auto-left">
-                        <div class="uk-grid-medium uk-child-width-auto uk-flex-middle" uk-grid="margin: uk-margin-small-top">
-                            <?php dynamic_sidebar("toolbar-right:cell") ?>
-                        </div>
-                    </div>
-                    <?php endif ?>
-
-                </div>
-            </div>
-            <?php endif ?>
-
+            <?php if (!($site['layout'] == 'boxed' && $site['boxed.header_outside'])) : ?>
             <?= get_view('header') ?>
+            <?php endif ?>
 
             <?php dynamic_sidebar("top:section") ?>
 
             <?php if (!$theme->get('builder')) : ?>
 
-            <div id="tm-main" class="tm-main uk-section uk-section-default" uk-height-viewport="expand: true">
-                <div class="uk-container">
+            <div id="tm-main" <?= get_attrs($attrs_main_section) ?> uk-height-viewport="expand: true">
+                <div<?= get_attrs($attrs_main_container) ?>>
 
-                    <?php
-                        $grid = ['uk-grid']; $sidebar = $theme->get('sidebar', []);
-                        $grid[] = $sidebar['gutter'] ? "uk-grid-{$sidebar['gutter']}" : '';
-                        $grid[] = $sidebar['divider'] ? "uk-grid-divider" : '';
+                    <?php if (is_active_sidebar('sidebar')) :
+                            $sidebar = $theme->get('sidebar', []);
+                            $grid = ['uk-grid'];
+                            $grid[] = $sidebar['gutter'] ? "uk-grid-{$sidebar['gutter']}" : '';
+                            $grid[] = $sidebar['divider'] ? 'uk-grid-divider' : '';
                     ?>
 
                     <div<?= get_attrs(['class' => $grid, 'uk-grid' => true]) ?>>
                         <div class="uk-width-expand@<?= $theme->get('sidebar.breakpoint') ?>">
+
+                    <?php endif ?>
 
                             <?php if ($site['breadcrumbs']) : ?>
                             <div class="uk-margin-medium-bottom">
