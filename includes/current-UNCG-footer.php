900d5014c46729e877cb0c520571a840176385ce
tdoanUNCG <48730552+tdoanUNCG@users.noreply.github.com>
2019-05-29 12:09:23 -0230

    Update current-UNCG-footer.php

--- includes/current-UNCG-footer.php
+++ includes/current-UNCG-footer.php
@@ -1,123 +1,60 @@
-Skip to main contentSkip to toolbar
-About WordPress
-Office of Research Integrity
-00 Comments in moderation
-New
-UpdraftPlus
-Howdy, admin
-Log Out
-Help
-Edit Themes
-The last rules update for the Wordfence Web Application Firewall was unsuccessful. The last successful update check was May 12, 2019 3:11 am, so this site may be missing new rules added since then. You may wait for the next automatic attempt at June 2, 2019 3:11 am or try to Manually Update by clicking the "Manually Refresh Rules" button below the Rules list.
+<?php
+/**
+ * OreTech Footer Template
+ */
 
 
-YOOtheme Child Theme: current-UNCG-footer.php (includes/current-UNCG-footer.php)
-Select theme to edit:   
 
-Theme Files
-This child theme inherits templates from a parent theme, YOOtheme.
-style.css
-functions.php
-css folder
-plugin-update-checker folder
-theme folder
-includes folder
-js folder
-bootstrap-wp-navwalker.php
-current-UNCG-footer.php
-custom-header.php
-customizer.php
-extras.php
-privacy-policy.php
-template-tags.php
-js folder
-less folder
-README.md
-footer.php
-header.php
-index.php
-fonts folder
-Selected file content:
-22
+?>
+<div id="footer-content" class="uk-section-default uk-section uk-section-xsmall">
+    <div class="uk-container">
+        <div class="uk-grid-margin uk-grid">
+            <div class="uk-width-expand@m">
+                <div class="footer-left">
+                    <p><a class="emergency" title="Spartan Alert" href="https://spartanalert.uncg.edu">Emergency Info</a></p>
+                    <?php 
+						global $uncg_unit_options;
+						$uncg_unit_settings = get_option( 'uncg_unit_options', $uncg_unit_options );
+						echo $uncg_unit_settings['addressHTML'];
+					?>
+                </div>
+            </div>
             
-23
             <div class="uk-grid-item-match uk-flex-middle uk-width-expand@m">
-24
                 <div class="uk-panel">            
-25
                     <div class="uk-margin uk-text-center footer-middle">
-26
                         <img class="el-image" alt data-src="/rincuncg/images/uncg_hrzntl_footer.svg" uk-img>    
-27
     
-28
                     </div>
-29
-​
-30
+
                 </div>
-31
             </div>
-32
-​
-33
+
             <div class="uk-grid-item-match uk-flex-middle uk-width-expand@m">
-34
                 <div class="uk-panel">            
-35
                     <div class="uk-margin footer-right">
-36
                         <div class="footer-social">
-37
                             <ul>
-38
                                 <li class="social"><a href="https://www.facebook.com/uncg1891/"><img src="/rincuncg/images/FB.svg" alt="Facebook"></a></li>
-39
                                 <li class="social"><a href="https://twitter.com/UNCG"><img src="/rincuncg/images/Twitter.svg" alt="Twitter"></a></li>
-40
                                 <li class="social"><a href="https://www.instagram.com/uncg/"><img src="/rincuncg/images/Instagram.svg" alt="Instagram"></a></li>
-41
                                 <li class="social"><a href="https://www.linkedin.com/school/uncg/"><img src="/rincuncg/images/Linkedin.svg" alt="Linkedin"></a></li>
-42
                                 <li class="social"><a href="https://www.youtube.com/uncg"><img src="/rincuncg/images/YT.svg" alt="Youtube"></a></li>
-43
                                 <li class="social"><a href="https://www.snapchat.com/add/uncgreensboro"><img src="/rincuncg/images/Snapchat.svg" alt="Snapchat"></a></li>
-44
                             </ul>
-45
                         </div>
-46
                         <img class="el-image" alt data-src="/rincuncg/images/unc_system_logo.svg" uk-img>    
-47
                     </div>
-48
                 </div>
-49
             </div>
-50
         </div>
-51
         <div class="uk-margin-remove-top uk-margin-remove-bottom uk-grid-margin uk-margin-remove-top uk-margin-remove-bottom" uk-grid>
-52
             <div class="uk-width-1-1@m">
-53
                 <div class="uk-margin uk-margin-remove-top uk-margin-remove-bottom uk-text-center">
-54
                     <p>Copyright © <?php echo date("Y"); ?>. UNC Greensboro. All rights reserved. | <a href="/privacy-policy">Privacy Policy</a></p>
-55
                 </div>
-56
             </div>
-57
         </div>
-58
     </div>
-59
     
-60
 </div>
-Documentation:  	
-	
-
-
-Thank you for creating with WordPress. Version 5.2.1
